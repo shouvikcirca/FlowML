b902f58d218750a4f01f5d9146f901548a936ca2
shouvikcirca <goldenfibo1123@gmail.com>
2022-06-23 18:27:33 +0530

    Setup training and mapping metrics for multiple runs with varying hyperparameters

--- baseRunner.py
+++ baseRunner.py
@@ -1,41 +1,31 @@
-#import os
-#import numpy as np
-#import tempfile
-#from mlflow.tracking.fluent import _get_experiment_id
-#import argparse
-#parser = argparse.ArgumentParser()
-#parser.parse_args()
-#parser.add_argument("--learningrate")
-#args = parser.parse_args()
-
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import StratifiedShuffleSplit
 import mlflow
 from mlflow.tracking.client import MlflowClient
 import click
+import mlflow.keras
+
+mlflow.keras.autolog(log_models = False)
 
 @click.command()
 @click.option("--learning-rate", default=0.1, type=float)
-def runBaseRunner(learning_rate):
+@click.option("--vocab-length", default=100, type=int)
+@click.option("--seq-padding-style", default="post", type=str)
+@click.option("--seq-truncating-style", default="post", type=str)
+@click.option("--embedding-dim", default=50, type=int)
+@click.option("--bs", default=64, type=int)
+@click.option("--epochs", default=5, type=int)
+@click.option("--max-length", default=50, type=int)
+def runBaseRunner(learning_rate, vocab_length, seq_padding_style, seq_truncating_style, embedding_dim, bs, epochs, max_length):
 	client = MlflowClient()
 	with mlflow.start_run() as active_run:
 		r = client.get_run(active_run.info.run_id)
-
-
-		client.log_param(r.info.run_id, "learningRate","Learning rate for this run is {}".format(learning_rate))
-
-
-
-if __name__ == "__main__":
-	runBaseRunner()
-
-
-
-
-
-
-
-"""
-	with mlflow.start_run() as active_run:
-	#Extracting comments and targets
+		
+		filePath = r.data.tags["dataPath"]
 		comments, targets = extractor(filePath)
 
 		#Converting to numpy arrays
@@ -43,12 +33,60 @@ if __name__ == "__main__":
 
 		targets = [i-1 for i in targets]
 		targets = np.array(targets)
-		
-		localFile = tempfile.mkdtemp()
-		Xfile = os.path.join(localFile,'comments.csv')
-		yfile = os.path.join(localFile, 'targets.csv')
-		mlflow.log_artifact("comments_{}.csv".format(bs), artifact_path="comments.csv")
-		mlflow.log_artifact("targets_{}.csv".format(bs), artifact_path="targets.csv")
-"""
 
+		model = tf.keras.Sequential([
+			tf.keras.layers.Embedding(vocab_length, embedding_dim, input_length=max_length),
+			tf.keras.layers.GlobalAveragePooling1D(),
+			tf.keras.layers.Dense(6, activation='relu'),
+			tf.keras.layers.Dense(4, activation='softmax')
+		])
+
+		model.compile(
+				loss='categorical_crossentropy', 
+				optimizer=tf.keras.optimizers.Adam(learning_rate = learning_rate), 
+				metrics=["accuracy"]
+		)
+		splitter = StratifiedShuffleSplit(n_splits = 3, test_size=0.2, random_state = 0)
+
+		for train_index, test_index in splitter.split(comments, targets):
+			trainX = comments[train_index]
+			trainY = targets[train_index]
+			testX = comments[test_index]
+			testY = targets[test_index]
+			tokenizer = Tokenizer(num_words=vocab_length, oov_token="<OOV>")
+			tokenizer.fit_on_texts(trainX) 
+			trainSequences = tokenizer.texts_to_sequences(trainX)
+			paddedTrainSequences = pad_sequences(trainSequences, padding=seq_padding_style, truncating=seq_truncating_style, maxlen=max_length)
+			testSequences = tokenizer.texts_to_sequences(testX)
+			paddedTestSequences = pad_sequences(testSequences, padding=seq_padding_style, truncating=seq_truncating_style, maxlen=max_length)
+			trainY = tf.keras.utils.to_categorical(trainY, 4)
+			testY = tf.keras.utils.to_categorical(testY, 4)
+			
+			history = model.fit(paddedTrainSequences, trainY, batch_size=bs, epochs=epochs, verbose=1)	
+			train_accuracy = model.history.history['accuracy'][-1]
+			val_accuracy = model.evaluate(paddedTestSequences, testY, batch_size=bs)[1]
+			
+			client.log_metric(run_id = active_run.info.run_id, key='trainAccuracy', value=train_accuracy)
+			client.log_metric(run_id = active_run.info.run_id, key='valAccuracy', value=val_accuracy)
+			client.set_terminated(active_run.info.run_id)
+			mlflow.keras.log_model(keras_model = model, artifact_path='artifact_{}'.format(active_run.info.run_id))
+
+
+def extractor(filePath):
+	files = [i for i in os.listdir(filePath) if '.txt' in i] # Only .txt files contain data
+	comments = []
+	targets = []
+	for f in files:
+		with open(filePath+f, 'r') as r:
+			s = r.read().split('\n')
+			s = [i.split('\t') for i in s]
+			comments.extend(s)	
+
+	targets = [int(item[0]) for item in comments if len(item) == 2]	
+	comments = [item[1] for item in comments if len(item) == 2]
+	return comments, targets
+
+
+if __name__ == "__main__":
+	runBaseRunner()
 
--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -1,16 +1,81 @@
 from mlflow.tracking.client import MlflowClient
 import mlflow
 
-def createRun(exp_id, learning_rate):
-	params = {
-			"learning_rate": learning_rate,
-		 }
+def createRun(hyperparams, exp_id):
+	client = MlflowClient()
+	run = client.create_run(exp_id, tags = {"dataPath":hyperparams["dataPath"]})
+	temp = hyperparams.pop("dataPath", None)
+	with mlflow.start_run(run_id=run.info.run_id, nested = True) as prun:
+		mlflow.log_params(hyperparams)
+	mlflow.run(run_id = run.info.run_id, uri = '.', entry_point = "ht", use_conda = False, parameters = hyperparams)
 
-	run = client.create_run(exp_id)
-	mlflow.run(run_id = run.info.run_id, uri = '.', entry_point = "ht", use_conda = False, parameters = params)
+
+def createExperimentAndDataArtifacts():
+	experiment_id = mlflow.create_experiment("HTExperiment")
+	return experiment_id
+
 
 if __name__ == '__main__':
-	client = MlflowClient()
-	for i in [0.1, 0.2, 0.3, 0.4, 0.5]:
-		createRun('2', i)
+	with mlflow.start_run(experiment_id = '0') as r:
+		hyperparams = [
+			{
+				"learning_rate":0.3,
+				"dataPath":'./',
+				"vocab_length":100,
+				"seq_padding_style":"post",
+				"seq_truncating_style":"post",
+				"embedding_dim":50,
+				"bs":64,
+				"epochs":5,
+				"max_length":50,
+			},
+			{
+				"learning_rate":0.4,
+				"dataPath":'./',
+				"vocab_length":100,
+				"seq_padding_style":"post",
+				"seq_truncating_style":"post",
+				"embedding_dim":50,
+				"bs":64,
+				"epochs":10,
+				"max_length":50,
+			},
+			{
+				"learning_rate":0.5,
+				"dataPath":'./',
+				"vocab_length":100,
+				"seq_padding_style":"post",
+				"seq_truncating_style":"post",
+				"embedding_dim":50,
+				"bs":64,
+				"epochs":15,
+				"max_length":50,
+			},
+			{
+				"learning_rate":0.6,
+				"dataPath":'./',
+				"vocab_length":100,
+				"seq_padding_style":"post",
+				"seq_truncating_style":"post",
+				"embedding_dim":50,
+				"bs":64,
+				"epochs":20,
+				"max_length":50,
+			},
+			{
+				"learning_rate":0.7,
+				"dataPath":'./',
+				"vocab_length":100,
+				"seq_padding_style":"post",
+				"seq_truncating_style":"post",
+				"embedding_dim":50,
+				"bs":64,
+				"epochs":25,
+				"max_length":50,
+			}
+		]
+
+		exp_id = createExperimentAndDataArtifacts()
+		for i in range(len(hyperparams)):
+				createRun(hyperparams[i], exp_id)
 
