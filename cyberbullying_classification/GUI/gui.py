c8987133439f2ff2f1880a158f54ebeb7c649dc8
shouvikcirca <goldenfibo1123@gmail.com>
2023-02-01 18:38:18 +0530

    Incorporated Model API call into GUI.

--- cyberbullying_classification/GUI/gui.py
+++ cyberbullying_classification/GUI/gui.py
@@ -1,44 +1,71 @@
+import pickle
+import os
 from tkinter import *
 import numpy as np
+from pymongo import MongoClient
+import mlflow
+from mlflow.tracking.client import MlflowClient
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+import requests
 
 root = Tk()
-verdicts = []
+
+connectionString = 'mongodb://root:password@localhost:27015/mlflowexperiments?authSource=admin'
+dbClient = MongoClient(connectionString)
+db = dbClient['mlflowexperiments']
+
+def sampleForActiveLearning(sample):
+	print('Sampled for AL:{}'.format(sample))
+
+def getPredictions(enteredText):
+	experimentName = 'Alpha'
+	client = MlflowClient()
+
+	collection = db['deployedModels']
+	runid = collection.find({'task':'cyberbullying_classification'})[0]['cyberbullying_classification']
+	
+	model_name = '{}_{}'.format(experimentName, runid)
+	stage='Staging'
+	modeluri = 'models:/{}/{}'.format(model_name, stage)
+	
+	local_dir = './tmp'
+	if not os.path.exists(local_dir):
+		os.mkdir(local_dir)
+	tokenizer_path = client.download_artifacts(runid, "tokenizer.pkl", local_dir)
+	tokenizer = pickle.load(open(tokenizer_path,'rb'))
+
+	comments = np.array([enteredText])
+	run = client.get_run(runid)
+	testSequences = tokenizer.texts_to_sequences(comments)
+	paddedTestSequences = pad_sequences(testSequences,padding = run.data.params['seq_padding_style'],truncating = run.data.params['seq_truncating_style'],maxlen = int(run.data.params['max_length']))
+
+	prediction = requests.post(url='http://127.0.0.1:5004/invocations',json={"inputs":paddedTestSequences.tolist()}, headers={'Content-Type':'application/json',})
+	p = np.array(prediction.json())
+
+	# p[0] contains the probability scores
+	return p[0]
 
 def predictClick():
 	listboxResults.delete(0, END)
 	enteredText = myLabel12.get()
 
-	probs = np.random.randint(10, size=(4))
-	obscene = probs[0]/probs.sum()
-	insulting = probs[1]/probs.sum()
-	hateful = probs[2]/probs.sum()
-	bullying = probs[3]/probs.sum()
+	obscene, insulting, hateful, bullying = preds = getPredictions(enteredText)
+
+	# If all the probability scores are below 0.8, sample is chosen for Active Learning
+	if not (preds>=0.8).sum():
+		sampleForActiveLearning(enteredText)
 
 	if obscene>0.5 or insulting>0.5 or hateful>0.5 or bullying>0.5:
 		enteredText = 'Disturbing Remark'
 
-	#verdicts.append(enteredText)
-	#renderedVerdicts = '\n'.join(verdicts)
-
-	
 	listboxResults.insert(END,'Obscene:{0:.2f}    '.format(obscene) )
 	listboxResults.insert(END,'Insulting:{0:.2f}    '.format(insulting) )
 	listboxResults.insert(END,'Hateful:{0:.2f}    '.format(hateful) )
 	listboxResults.insert(END,'Bullying:{0:.2f}    '.format(bullying) )
 	
 	myListbox32.insert(END,enteredText)
-	
-	#myLabel30 = Label(root, text='Obscene:{0:.2f}    '.format(obscene))
-	#myLabel31 = Label(root, text='Insulting:{0:.2f}    '.format(insulting))
-	
-	#myListbox32.insert(END, enteredText)
-	#myLabel33 = Label(root, text='Hateful:{0:.2f}    '.format(hateful))
-	#myLabel34 = Label(root, text='Bullying:{0:.2f}    '.format(bullying))
-	
-	#myLabel30.grid(row = 3, column = 0)
-	#myLabel31.grid(row = 3, column = 1)
-	#myLabel33.grid(row = 3, column = 3)
-	#myLabel34.grid(row = 3, column = 4)
+
 
 myLabel00 = Label(root, text="                    ")
 myLabel01 = Label(root, text="                    ")
@@ -58,7 +85,6 @@ myLabel22 = Button(root, text = "Predict", command = predictClick)
 myLabel23 = Label(root, text="                    ")
 myLabel24 = Label(root, text="                    ")
 
-#myLabel.pack()
 myLabel00.grid(row = 0, column = 0)
 myLabel01.grid(row = 0, column = 1)
 myLabel02.grid(row = 0, column = 2)
@@ -83,7 +109,7 @@ my_scrollbar.pack(side=RIGHT, fill=Y)
 listboxResults = Listbox(myFrame32, width = 25)
 myFrame32.grid(row=3, column=2, pady = (0,10))
 
-listboxResults.pack(side=LEFT)#grid(row = 0, column = 0)
-myListbox32.pack(side=RIGHT)#grid(row = 0, column = 1)
+listboxResults.pack(side=LEFT)
+myListbox32.pack(side=RIGHT)
 
 root.mainloop()
